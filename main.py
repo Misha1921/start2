163c29c8317f179e8f8ef81eef1474dc9b451ed2
Misha1921 <82591954+Misha1921@users.noreply.github.com>
2023-11-07 13:41:04 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,65 +1,3 @@
-# # class Parent:
-# #     pass
-# #
-# # class Child(Parent):
-# #     pass
-# #
-# # class Human:
-# #     height = 170;
-# #
-# # class Student(Human):
-# #     salary = 400
-# #
-# # class Worker(Human):
-# #     salary = 15000
-# #
-# # nick = Student()
-# # Anna = Worker()
-# # print(nick.height)
-# # print(nick.salary)
-# # print("----")
-# # print(Anna.height)
-# # print(Anna.salary)
-# #
-# #
-# #
-# # class GrandParent:
-# #     height = 170
-# #     satiety = 100
-# #     age = 60
-# #
-# # class Parent(GrandParent):
-# #     height = 170
-# #     age = 40
-# #
-# # class Child(Parent):
-# #     height = 50
-# #     def __init__(self):
-# #         print(self.height)
-# #         print(self.satienty)
-# #         print(self.age)
-# # nick = Child()
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-# class Hello:
-#     def __init__(self):
-#         print("hello")
-# class HelloWorld(Hello)
-#     def __init__(self):
-#         super().__init__()
-#         print("World")
-# one = HelloWorld()
-
-
 class GrandParent:
     def about(self):
         print("I am GrandParent")
@@ -76,4 +14,4 @@ class Child(Parent):
         super().About()
         super().AboutMyself()
 
-Jamal = Child()
+Jamil = Child()
